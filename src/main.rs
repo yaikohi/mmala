31d853477d57a2810cf6b4ec76222d800f86c38c
ykhi <erik.beem@hotmail.com>
2025-03-28 16:45:40 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+            .route("/", web::get().to(hello_world))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
